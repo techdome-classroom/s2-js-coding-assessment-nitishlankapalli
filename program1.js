9deec131f9a7ddb72545cfaf0e91361126634b4c
Raju Muddu <rajumuddu@Rajus-MacBook-Air.local>
2024-11-11 14:58:55 +0530

    submitting code

--- program1.js
+++ program1.js
@@ -3,7 +3,28 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const matchingBrackets = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    const stack = [];
+
+    for (let char of s) {
+        if (char in matchingBrackets) {
+
+            const topElement = stack.length > 0 ? stack.pop() : '#';
+            if (matchingBrackets[char] !== topElement) {
+                return false;
+            }
+        } else {
+
+            stack.push(char);
+        }
+    }
+
+
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
--- program2.js
+++ program2.js
@@ -3,7 +3,32 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const currentVal = romanValues[s[i]];
+        const nextVal = romanValues[s[i + 1]];
+
+        if (nextVal > currentVal) {
+
+            total -= currentVal;
+        } else {
+            
+            total += currentVal;
+        }
+    }
+
+    return total;
 };
 
 
